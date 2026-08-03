051c3e2c9981d2c07a86f972d71108ee39cb23b6
wcrean <bill.crean@yahoo.com>
2026-08-03 15:13:54 -0400

    Deploy Men’s Trip Trivia Phase 1B proof of concept

--- firebase-config.js
+++ firebase-config.js
@@ -2,11 +2,12 @@
 // This client-side configuration is intended to be published with the web app.
 // Security is enforced by Firebase Authentication and Firestore Security Rules.
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyD5f86Wz10WUaqrISWhe-QH25pvVxhCuhk",
-  authDomain: "search-party-game.firebaseapp.com",
-  projectId: "search-party-game",
-  storageBucket: "search-party-game.firebasestorage.app",
-  messagingSenderId: "892714848487",
-  appId: "1:892714848487:web:c98d84938c76675d5e5242"
+const firebaseConfig = {
+  apiKey: "AIzaSyCinGDDS6hlR6wjRJ1jZpvm8Y3s2PCMOMs",
+  authDomain: "mens-trip-trivia.firebaseapp.com",
+  projectId: "mens-trip-trivia",
+  storageBucket: "mens-trip-trivia.firebasestorage.app",
+  messagingSenderId: "507253563413",
+  appId: "1:507253563413:web:3d8c431f036b569aa96e1c",
+  measurementId: "G-KKVL7VSJJJ"
 };
